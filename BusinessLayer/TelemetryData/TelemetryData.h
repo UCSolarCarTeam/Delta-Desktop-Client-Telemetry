777ed5fcb64350c9fc0572b4fbcd3cf6fb2d67ee
Enoch Tsang <ectsang@ucalgary.ca>
2015-01-22 21:40:12 -0700

    removed old functionality from telemetrydata, migrated to other data

--- BusinessLayer/TelemetryData/TelemetryData.h
+++ BusinessLayer/TelemetryData/TelemetryData.h
@@ -7,52 +7,6 @@ class TelemetryData : public I_TelemetryData
 public:
   TelemetryData();
   virtual ~TelemetryData();
-
-  /*Data "Gets"*/
-  double driverSetSpeedRPM() const;
-  double driverSetCurrent() const;
-
-  double vehicleVelocityKph() const;
-
-  double motorVelocityRpm() const;
-
-  
-  double ipmHeatSinkTemp() const;
-  double dspBoardTemp() const;
-
-
-  double receivedErrorCount() const;
-  double transmittedErrorCount() const;
-
-
-
-   /*Data "Sets"*/
-   void setDriverSetSpeedRPM(double driverSetSpeedRPM);
-   void setDriverSetCurrent(double driverSetCurrent);
-
-   void setVehicleVelocityKph(double vehicleVelocityKph);
-
-   void setMotorVelocityRpm(double motorVelocityRpm);
-
-   void setIpmHeatSinkTemp(double ipmHeatSinkTemp);
-   void setDspBoardTemp(double dspBoardTemp);
-
-   void setReceivedErrorCount(double receivedErrorCount);
-   void setTransmittedErrorCount(double transmittedErrorCount);
-
-private:
-   double driverSetSpeedRPM_;
-   double driverSetCurrent_;
-
-   double vehicleVelocityKph_;
-
-   double motorVelocityRpm_;
- 
-   double ipmHeatSinkTemp_;
-   double dspBoardTemp_;
-   
-   double receivedErrorCount_;
-   double transmittedErrorCount_;
 };
 
 
