4b8456714ad4fe9b46b6cf0c1c8bf3b9ba6cebc5
Enoch Tsang <ectsang@ucalgary.ca>
2015-04-04 15:18:23 -0600

    added access functions for MpptUI and FaultsUI

--- ViewLayer/FaultsUI/FaultsUI.cpp
+++ ViewLayer/FaultsUI/FaultsUI.cpp
@@ -16,3 +16,118 @@ void FaultsUI::hideHeaderBar()
 {
 	ui->faultsTitleBar->hide();
 }
+
+
+QLabel& FaultsUI::cellOverVoltageLabel()
+{
+	return *ui->cellOverVoltageLabel;
+}
+QLabel& FaultsUI::bmuInSetupModeLabel()
+{
+	return *ui->bmuInSetupModeLabel;
+}
+QLabel& FaultsUI::cellOverTemperatureLabel()
+{
+	return *ui->cellOverTemperatureLabel;
+}
+QLabel& FaultsUI::softwareOverCurrentMeasurementNotValidLabel()
+{
+	return *ui->softwareOverCurrentMeasurementNotValidLabel;
+}
+QLabel& FaultsUI::cellUnderVoltageLabel()
+{
+	return *ui->cellUnderVoltageLabel;
+}
+QLabel& FaultsUI::can12vSupplyLowLabel()
+{
+	return *ui->can12vSupplyLowLabel;
+}
+QLabel& FaultsUI::vehicleCommunicationsTimeoutLabel()
+{
+	return *ui->vehicleCommunicationsTimeoutLabel;
+}
+QLabel& FaultsUI::packIsolationTestFailureLabel()
+{
+	return *ui->packIsolationTestFailureLabel;
+}
+QLabel& FaultsUI::contactorStuckOrNotEngagedLabel()
+{
+	return *ui->contactorStuckOrNotEngagedLabel;
+}
+QLabel& FaultsUI::cmuCanBusPowerStatusLabel()
+{
+	return *ui->cmuCanBusPowerStatusLabel;
+}
+QLabel& FaultsUI::measurementUntrustedLabel()
+{
+	return *ui->measurementUntrustedLabel;
+}
+QLabel& FaultsUI::cmuDetectedExtraCellLabel()
+{
+	return *ui->cmuDetectedExtraCellLabel;
+}
+QLabel& FaultsUI::ipmTemperatureOrMotorTemperatureLabel()
+{
+	return *ui->ipmTemperatureOrMotorTemperatureLabel;
+}
+QLabel& FaultsUI::busVoltageUpperLimitLabel()
+{
+	return *ui->busVoltageUpperLimitLabel;
+}
+QLabel& FaultsUI::motorCurrentLabel()
+{
+	return *ui->motorCurrentLabel;
+}
+QLabel& FaultsUI::busVoltageLowerLimitLabel()
+{
+	return *ui->busVoltageLowerLimitLabel;
+}
+QLabel& FaultsUI::outputVoltagePwmLabel()
+{
+	return *ui->outputVoltagePwmLabel;
+}
+QLabel& FaultsUI::busCurrentLabel()
+{
+	return *ui->busCurrentLabel;
+}
+QLabel& FaultsUI::velocityLabel()
+{
+	return *ui->velocityLabel;
+}
+QLabel& FaultsUI::desaturationFaultLabel()
+{
+	return *ui->desaturationFaultLabel;
+}
+QLabel& FaultsUI::badMotorPositionHallSequence()
+{
+	return *ui->badMotorPositionHallSequence;
+}
+QLabel& FaultsUI::fifteenVoltRailUnderVoltageLockoutLabel()
+{
+	return *ui->fifteenVoltRailUnderVoltageLockoutLabel;
+}
+QLabel& FaultsUI::configReadErrorLabel()
+{
+	return *ui->configReadErrorLabel;
+}
+QLabel& FaultsUI::watchDogCausedLastResetLabel()
+{
+	return *ui->watchDogCausedLastResetLabel;
+}
+QLabel& FaultsUI::dcBusOverVoltageLabel()
+{
+	return *ui->dcBusOverVoltageLabel;
+}
+QLabel& FaultsUI::softwareOverCurrentLabel()
+{
+	return *ui->softwareOverCurrentLabel;
+}
+QLabel& FaultsUI::hardwareOverCurrentLabel()
+{
+	return *ui->hardwareOverCurrentLabel;
+}
+
+QTextBrowser& FaultsUI::historyTextBrowser()
+{
+	return *ui->historyTextBrowser;
+}
--- ViewLayer/MpptUI/MpptUI.h
+++ ViewLayer/MpptUI/MpptUI.h
@@ -16,6 +16,90 @@ public:
 
     void hideHeaderBar();
 
+	QLabel& setMppt1PowerIn();
+	QLabel& setMppt1VoltageIn();
+	QLabel& setMppt1CurrentIn();
+	QLabel& setMppt1PowerOut();
+	QLabel& setMppt1VoltageOut();
+	QLabel& setMppt1CurrentOut();
+	QLabel& setMppt1Efficiency();
+
+	QLabel& setMppt2PowerIn();
+	QLabel& setMppt2VoltageIn();
+	QLabel& setMppt2CurrentIn();
+	QLabel& setMppt2PowerOut();
+	QLabel& setMppt2VoltageOut();
+	QLabel& setMppt2CurrentOut();
+	QLabel& setMppt2Efficiency();
+
+	QLabel& setMppt3PowerIn();
+	QLabel& setMppt3VoltageIn();
+	QLabel& setMppt3CurrentIn();
+	QLabel& setMppt3PowerOut();
+	QLabel& setMppt3VoltageOut();
+	QLabel& setMppt3CurrentOut();
+	QLabel& setMppt3Efficiency();
+
+	QLabel& setMppt4PowerIn();
+	QLabel& setMppt4VoltageIn();
+	QLabel& setMppt4CurrentIn();
+	QLabel& setMppt4PowerOut();
+	QLabel& setMppt4VoltageOut();
+	QLabel& setMppt4CurrentOut();
+	QLabel& setMppt4Efficiency();
+
+	QLabel& setMppt5PowerIn();
+	QLabel& setMppt5VoltageIn();
+	QLabel& setMppt5CurrentIn();
+	QLabel& setMppt5PowerOut();
+	QLabel& setMppt5VoltageOut();
+	QLabel& setMppt5CurrentOut();
+	QLabel& setMppt5Efficiency();
+
+	QLabel& setMppt6PowerIn();
+	QLabel& setMppt6VoltageIn();
+	QLabel& setMppt6CurrentIn();
+	QLabel& setMppt6PowerOut();
+	QLabel& setMppt6VoltageOut();
+	QLabel& setMppt6CurrentOut();
+	QLabel& setMppt6Efficiency();
+
+	QLabel& setPower1PowerValue();
+	QLabel& setPower1VoltageValue();
+	QLabel& setPower1CurrentValue();
+
+	QLabel& setPower2PowerValue();
+	QLabel& setPower2VoltageValue();
+	QLabel& setPower2CurrentValue();
+
+	QLabel& setPower3PowerValue();
+	QLabel& setPower3VoltageValue();
+	QLabel& setPower3CurrentValue();
+
+	QLabel& setPower4PowerValue();
+	QLabel& setPower4VoltageValue();
+	QLabel& setPower4CurrentValue();
+
+	QLabel& setPower5PowerValue();
+	QLabel& setPower5VoltageValue();
+	QLabel& setPower5CurrentValue();
+
+	QLabel& setPower6PowerValue();
+	QLabel& setPower6VoltageValue();
+	QLabel& setPower6CurrentValue();
+
+	QWebView& setGraph1();
+	QWebView& setGraph2();
+	QWebView& setGraph3();
+	QComboBox& getGraphSelector1();
+	QComboBox& getGraphSelector2();
+	QComboBox& getGraphSelector3();
+
+	QGraphicsView& setGraphicalViewContent();
+	QComboBox& getGraphicalViewSelector();
+	QComboBox& getAlgorithmSelector();
+	QPushButton& commsSendButton();
+
 private:
 	Ui::MpptUI *ui;
 };
