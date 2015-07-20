0fb1f09b8b9620128becd7fa80b097af1a700cdd
Enoch Tsang <etsang1@hotmail.com>
2015-07-20 01:39:03 -0600

    attempt to fix mppt power and efficiency values

--- ViewLayer/MpptView/MpptView.cpp
+++ ViewLayer/MpptView/MpptView.cpp
@@ -101,38 +101,148 @@ MpptView::MpptView(MpptPresenter& presenter, MpptUI& ui)
 MpptView::~MpptView()
 {
 }
+void MpptView::updateMppt1PowerIn()
+{
+   double powerIn = ui_.setMppt1CurrentIn().text().toDouble() * ui_.setMppt1PowerIn().text().toDouble();
+   ui_.setMppt1PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt1PowerOut()
+{
+   double powerOut = ui_.setMppt1CurrentOut().text().toDouble() * ui_.setMppt1PowerOut().text().toDouble();
+   ui_.setMppt1PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt1Efficiency()
+{
+   double powerIn = ui_.setMppt1PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt1PowerOut().text().toDouble();
+   ui_.setMppt1Efficiency().setNum(powerOut / powerIn * 100);
+}
+
+void MpptView::updateMppt2PowerIn()
+{
+   double powerIn = ui_.setMppt2CurrentIn().text().toDouble() * ui_.setMppt2PowerIn().text().toDouble();
+   ui_.setMppt2PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt2PowerOut()
+{
+   double powerOut = ui_.setMppt2CurrentOut().text().toDouble() * ui_.setMppt2PowerOut().text().toDouble();
+   ui_.setMppt2PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt2Efficiency()
+{
+   double powerIn = ui_.setMppt2PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt2PowerOut().text().toDouble();
+   ui_.setMppt2Efficiency().setNum(powerOut / powerIn * 100);
+}
+
+void MpptView::updateMppt3PowerIn()
+{
+   double powerIn = ui_.setMppt3CurrentIn().text().toDouble() * ui_.setMppt3PowerIn().text().toDouble();
+   ui_.setMppt3PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt3PowerOut()
+{
+   double powerOut = ui_.setMppt3CurrentOut().text().toDouble() * ui_.setMppt3PowerOut().text().toDouble();
+   ui_.setMppt3PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt3Efficiency()
+{
+   double powerIn = ui_.setMppt3PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt3PowerOut().text().toDouble();
+   ui_.setMppt3Efficiency().setNum(powerOut / powerIn * 100);
+}
+
+void MpptView::updateMppt4PowerIn()
+{
+   double powerIn = ui_.setMppt4CurrentIn().text().toDouble() * ui_.setMppt4PowerIn().text().toDouble();
+   ui_.setMppt4PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt4PowerOut()
+{
+   double powerOut = ui_.setMppt4CurrentOut().text().toDouble() * ui_.setMppt4PowerOut().text().toDouble();
+   ui_.setMppt4PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt4Efficiency()
+{
+   double powerIn = ui_.setMppt4PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt4PowerOut().text().toDouble();
+   ui_.setMppt4Efficiency().setNum(powerOut / powerIn * 100);
+}
+
+void MpptView::updateMppt5PowerIn()
+{
+   double powerIn = ui_.setMppt5CurrentIn().text().toDouble() * ui_.setMppt5PowerIn().text().toDouble();
+   ui_.setMppt5PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt5PowerOut()
+{
+   double powerOut = ui_.setMppt5CurrentOut().text().toDouble() * ui_.setMppt5PowerOut().text().toDouble();
+   ui_.setMppt5PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt5Efficiency()
+{
+   double powerIn = ui_.setMppt5PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt5PowerOut().text().toDouble();
+   ui_.setMppt5Efficiency().setNum(powerOut / powerIn * 100);
+}
+
+void MpptView::updateMppt6PowerIn()
+{
+   double powerIn = ui_.setMppt6CurrentIn().text().toDouble() * ui_.setMppt6PowerIn().text().toDouble();
+   ui_.setMppt6PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt6PowerOut()
+{
+   double powerOut = ui_.setMppt6CurrentOut().text().toDouble() * ui_.setMppt6PowerOut().text().toDouble();
+   ui_.setMppt6PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt6Efficiency()
+{
+   double powerIn = ui_.setMppt6PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt6PowerOut().text().toDouble();
+   ui_.setMppt6Efficiency().setNum(powerOut / powerIn * 100);
+}
+
+void MpptView::updateMppt7PowerIn()
+{
+   double powerIn = ui_.setMppt7CurrentIn().text().toDouble() * ui_.setMppt7PowerIn().text().toDouble();
+   ui_.setMppt7PowerIn().setNum(powerIn);
+}
+void MpptView::updateMppt7PowerOut()
+{
+   double powerOut = ui_.setMppt7CurrentOut().text().toDouble() * ui_.setMppt7PowerOut().text().toDouble();
+   ui_.setMppt7PowerIn().setNum(powerOut);
+}
+void MpptView::updateMppt7Efficiency()
+{
+   double powerIn = ui_.setMppt7PowerIn().text().toDouble();
+   double powerOut = ui_.setMppt7PowerOut().text().toDouble();
+   ui_.setMppt7Efficiency().setNum(powerOut / powerIn * 100);
+}
 
 void MpptView::mppt1VoltageInReceived(double mppt1VoltageIn)
 {
    ui_.setMppt1VoltageIn().setNum(mppt1VoltageIn);
-   // ui_.setMppt1PowerIn().setNum(mppt1VoltageIn *
-      // ui_.setMppt1CurrentIn().text().toDouble());
-   // ui_.setMppt1Efficiency().setNum(ui_.setMppt1PowerIn().text().toDouble() *
-      // ui_.setMppt1PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt1PowerIn();
+   updateMppt1Efficiency();
 }
 void MpptView::mppt1CurrentInReceived(double mppt1CurrentIn)
 {
    ui_.setMppt1CurrentIn().setNum(mppt1CurrentIn);
-   // ui_.setMppt1PowerIn().setNum(mppt1CurrentIn *
-      // ui_.setMppt1VoltageIn().text().toDouble());
-   // ui_.setMppt1Efficiency().setNum(ui_.setMppt1PowerIn().text().toDouble() *
-      // ui_.setMppt1PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt1PowerIn();
+   updateMppt1Efficiency();
 }
 void MpptView::mppt1VoltageOutReceived(double mppt1VoltageOut)
 {
    ui_.setMppt1VoltageOut().setNum(mppt1VoltageOut);
-   // ui_.setMppt1PowerOut().setNum(mppt1VoltageOut *
-      // ui_.setMppt1CurrentOut().text().toDouble());
-   // ui_.setMppt1Efficiency().setNum(ui_.setMppt1PowerIn().text().toDouble() *
-      // ui_.setMppt1PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt1PowerOut();
+   updateMppt1Efficiency();
 }
 void MpptView::mppt1CurrentOutReceived(double mppt1CurrentOut)
 {
    ui_.setMppt1CurrentOut().setNum(mppt1CurrentOut);
-   // ui_.setMppt1PowerOut().setNum(mppt1CurrentOut *
-      // ui_.setMppt1VoltageOut().text().toDouble());
-   // ui_.setMppt1Efficiency().setNum(ui_.setMppt1PowerIn().text().toDouble() *
-      // ui_.setMppt1PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt1PowerOut();
+   updateMppt1Efficiency();
 }
 void MpptView::mppt1TypeReceived(QString mppt1Type)
 {
@@ -146,34 +256,26 @@ void MpptView::mppt1ModeReceived(QString mppt1Mode)
 void MpptView::mppt2VoltageInReceived(double mppt2VoltageIn)
 {
    ui_.setMppt2VoltageIn().setNum(mppt2VoltageIn);
-   // ui_.setMppt2PowerIn().setNum(mppt2VoltageIn *
-      // ui_.setMppt2CurrentIn().text().toDouble());
-   // ui_.setMppt2Efficiency().setNum(ui_.setMppt2PowerIn().text().toDouble() *
-      // ui_.setMppt2PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt2PowerIn();
+   updateMppt2Efficiency();
 }
 void MpptView::mppt2CurrentInReceived(double mppt2CurrentIn)
 {
    ui_.setMppt2CurrentIn().setNum(mppt2CurrentIn);
-   // ui_.setMppt2PowerIn().setNum(mppt2CurrentIn *
-      // ui_.setMppt2VoltageIn().text().toDouble());
-   // ui_.setMppt2Efficiency().setNum(ui_.setMppt2PowerIn().text().toDouble() *
-      // ui_.setMppt2PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt2PowerIn();
+   updateMppt2Efficiency();
 }
 void MpptView::mppt2VoltageOutReceived(double mppt2VoltageOut)
 {
    ui_.setMppt2VoltageOut().setNum(mppt2VoltageOut);
-   // ui_.setMppt2PowerOut().setNum(mppt2VoltageOut *
-      // ui_.setMppt2CurrentOut().text().toDouble());
-   // ui_.setMppt2Efficiency().setNum(ui_.setMppt2PowerIn().text().toDouble() *
-      // ui_.setMppt2PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt2PowerOut();
+   updateMppt2Efficiency();
 }
 void MpptView::mppt2CurrentOutReceived(double mppt2CurrentOut)
 {
    ui_.setMppt2CurrentOut().setNum(mppt2CurrentOut);
-   // ui_.setMppt2PowerOut().setNum(mppt2CurrentOut *
-      // ui_.setMppt2VoltageOut().text().toDouble());
-   // ui_.setMppt2Efficiency().setNum(ui_.setMppt2PowerIn().text().toDouble() *
-      // ui_.setMppt2PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt2PowerOut();
+   updateMppt2Efficiency();
 }
 void MpptView::mppt2TypeReceived(QString mppt2Type)
 {
@@ -187,34 +289,26 @@ void MpptView::mppt2ModeReceived(QString mppt2Mode)
 void MpptView::mppt3VoltageInReceived(double mppt3VoltageIn)
 {
    ui_.setMppt3VoltageIn().setNum(mppt3VoltageIn);
-   // ui_.setMppt3PowerIn().setNum(mppt3VoltageIn *
-      // ui_.setMppt3CurrentIn().text().toDouble());
-   // ui_.setMppt3Efficiency().setNum(ui_.setMppt3PowerIn().text().toDouble() *
-      // ui_.setMppt3PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt3PowerIn();
+   updateMppt3Efficiency();
 }
 void MpptView::mppt3CurrentInReceived(double mppt3CurrentIn)
 {
    ui_.setMppt3CurrentIn().setNum(mppt3CurrentIn);
-   // ui_.setMppt3PowerIn().setNum(mppt3CurrentIn *
-      // ui_.setMppt3VoltageIn().text().toDouble());
-   // ui_.setMppt3Efficiency().setNum(ui_.setMppt3PowerIn().text().toDouble() *
-      // ui_.setMppt3PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt3PowerIn();
+   updateMppt3Efficiency();
 }
 void MpptView::mppt3VoltageOutReceived(double mppt3VoltageOut)
 {
    ui_.setMppt3VoltageOut().setNum(mppt3VoltageOut);
-   // ui_.setMppt3PowerOut().setNum(mppt3VoltageOut *
-      // ui_.setMppt3CurrentOut().text().toDouble());
-   // ui_.setMppt3Efficiency().setNum(ui_.setMppt3PowerIn().text().toDouble() *
-      // ui_.setMppt3PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt3PowerOut();
+   updateMppt3Efficiency();
 }
 void MpptView::mppt3CurrentOutReceived(double mppt3CurrentOut)
 {
    ui_.setMppt3CurrentOut().setNum(mppt3CurrentOut);
-   // ui_.setMppt3PowerOut().setNum(mppt3CurrentOut *
-      // ui_.setMppt3VoltageOut().text().toDouble());
-   // ui_.setMppt3Efficiency().setNum(ui_.setMppt3PowerIn().text().toDouble() *
-      // ui_.setMppt3PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt3PowerOut();
+   updateMppt3Efficiency();
 }
 void MpptView::mppt3TypeReceived(QString mppt3Type)
 {
@@ -228,34 +322,26 @@ void MpptView::mppt3ModeReceived(QString mppt3Mode)
 void MpptView::mppt4VoltageInReceived(double mppt4VoltageIn)
 {
    ui_.setMppt4VoltageIn().setNum(mppt4VoltageIn);
-   // ui_.setMppt4PowerIn().setNum(mppt4VoltageIn *
-      // ui_.setMppt4CurrentIn().text().toDouble());
-   // ui_.setMppt4Efficiency().setNum(ui_.setMppt4PowerIn().text().toDouble() *
-      // ui_.setMppt4PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt4PowerIn();
+   updateMppt4Efficiency();
 }
 void MpptView::mppt4CurrentInReceived(double mppt4CurrentIn)
 {
    ui_.setMppt4CurrentIn().setNum(mppt4CurrentIn);
-   // ui_.setMppt4PowerIn().setNum(mppt4CurrentIn *
-      // ui_.setMppt4VoltageIn().text().toDouble());
-   // ui_.setMppt4Efficiency().setNum(ui_.setMppt4PowerIn().text().toDouble() *
-      // ui_.setMppt4PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt4PowerIn();
+   updateMppt4Efficiency();
 }
 void MpptView::mppt4VoltageOutReceived(double mppt4VoltageOut)
 {
    ui_.setMppt4VoltageOut().setNum(mppt4VoltageOut);
-   // ui_.setMppt4PowerOut().setNum(mppt4VoltageOut *
-      // ui_.setMppt4CurrentOut().text().toDouble());
-   // ui_.setMppt4Efficiency().setNum(ui_.setMppt4PowerIn().text().toDouble() *
-      // ui_.setMppt4PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt4PowerOut();
+   updateMppt4Efficiency();
 }
 void MpptView::mppt4CurrentOutReceived(double mppt4CurrentOut)
 {
    ui_.setMppt4CurrentOut().setNum(mppt4CurrentOut);
-   // ui_.setMppt4PowerOut().setNum(mppt4CurrentOut *
-      // ui_.setMppt4VoltageOut().text().toDouble());
-   // ui_.setMppt4Efficiency().setNum(ui_.setMppt4PowerIn().text().toDouble() *
-      // ui_.setMppt4PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt4PowerOut();
+   updateMppt4Efficiency();
 }
 void MpptView::mppt4TypeReceived(QString mppt4Type)
 {
@@ -269,34 +355,26 @@ void MpptView::mppt4ModeReceived(QString mppt4Mode)
 void MpptView::mppt5VoltageInReceived(double mppt5VoltageIn)
 {
    ui_.setMppt5VoltageIn().setNum(mppt5VoltageIn);
-   // ui_.setMppt5PowerIn().setNum(mppt5VoltageIn *
-      // ui_.setMppt5CurrentIn().text().toDouble());
-   // ui_.setMppt5Efficiency().setNum(ui_.setMppt5PowerIn().text().toDouble() *
-      // ui_.setMppt5PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt5PowerIn();
+   updateMppt5Efficiency();
 }
 void MpptView::mppt5CurrentInReceived(double mppt5CurrentIn)
 {
    ui_.setMppt5CurrentIn().setNum(mppt5CurrentIn);
-   // ui_.setMppt5PowerIn().setNum(mppt5CurrentIn *
-      // ui_.setMppt5VoltageIn().text().toDouble());
-   // ui_.setMppt5Efficiency().setNum(ui_.setMppt5PowerIn().text().toDouble() *
-      // ui_.setMppt5PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt5PowerIn();
+   updateMppt5Efficiency();
 }
 void MpptView::mppt5VoltageOutReceived(double mppt5VoltageOut)
 {
    ui_.setMppt5VoltageOut().setNum(mppt5VoltageOut);
-   // ui_.setMppt5PowerOut().setNum(mppt5VoltageOut *
-      // ui_.setMppt5CurrentOut().text().toDouble());
-   // ui_.setMppt5Efficiency().setNum(ui_.setMppt5PowerIn().text().toDouble() *
-      // ui_.setMppt5PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt5PowerOut();
+   updateMppt5Efficiency();
 }
 void MpptView::mppt5CurrentOutReceived(double mppt5CurrentOut)
 {
    ui_.setMppt5CurrentOut().setNum(mppt5CurrentOut);
-   // ui_.setMppt5PowerOut().setNum(mppt5CurrentOut *
-      // ui_.setMppt5VoltageOut().text().toDouble());
-   // ui_.setMppt5Efficiency().setNum(ui_.setMppt5PowerIn().text().toDouble() *
-      // ui_.setMppt5PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt5PowerOut();
+   updateMppt5Efficiency();
 }
 void MpptView::mppt5TypeReceived(QString mppt5Type)
 {
@@ -310,34 +388,26 @@ void MpptView::mppt5ModeReceived(QString mppt5Mode)
 void MpptView::mppt6VoltageInReceived(double mppt6VoltageIn)
 {
    ui_.setMppt6VoltageIn().setNum(mppt6VoltageIn);
-   // ui_.setMppt6PowerIn().setNum(mppt6VoltageIn *
-      // ui_.setMppt6CurrentIn().text().toDouble());
-   // ui_.setMppt6Efficiency().setNum(ui_.setMppt6PowerIn().text().toDouble() *
-      // ui_.setMppt6PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt6PowerIn();
+   updateMppt6Efficiency();
 }
 void MpptView::mppt6CurrentInReceived(double mppt6CurrentIn)
 {
    ui_.setMppt6CurrentIn().setNum(mppt6CurrentIn);
-   // ui_.setMppt6PowerIn().setNum(mppt6CurrentIn *
-      // ui_.setMppt6VoltageIn().text().toDouble());
-   // ui_.setMppt6Efficiency().setNum(ui_.setMppt6PowerIn().text().toDouble() *
-      // ui_.setMppt6PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt6PowerIn();
+   updateMppt6Efficiency();
 }
 void MpptView::mppt6VoltageOutReceived(double mppt6VoltageOut)
 {
    ui_.setMppt6VoltageOut().setNum(mppt6VoltageOut);
-   // ui_.setMppt6PowerOut().setNum(mppt6VoltageOut *
-      // ui_.setMppt6CurrentOut().text().toDouble());
-   // ui_.setMppt6Efficiency().setNum(ui_.setMppt6PowerIn().text().toDouble() *
-      // ui_.setMppt6PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt6PowerOut();
+   updateMppt6Efficiency();
 }
 void MpptView::mppt6CurrentOutReceived(double mppt6CurrentOut)
 {
    ui_.setMppt6CurrentOut().setNum(mppt6CurrentOut);
-   // ui_.setMppt6PowerOut().setNum(mppt6CurrentOut *
-      // ui_.setMppt6VoltageOut().text().toDouble());
-   // ui_.setMppt6Efficiency().setNum(ui_.setMppt6PowerIn().text().toDouble() *
-      // ui_.setMppt6PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt6PowerOut();
+   updateMppt6Efficiency();
 }
 void MpptView::mppt6TypeReceived(QString mppt6Type)
 {
@@ -351,34 +421,26 @@ void MpptView::mppt6ModeReceived(QString mppt6Mode)
 void MpptView::mppt7VoltageInReceived(double mppt7VoltageIn)
 {
    ui_.setMppt7VoltageIn().setNum(mppt7VoltageIn);
-   // ui_.setMppt7PowerIn().setNum(mppt7VoltageIn *
-      // ui_.setMppt7CurrentIn().text().toDouble());
-   // ui_.setMppt7Efficiency().setNum(ui_.setMppt7PowerIn().text().toDouble() *
-      // ui_.setMppt7PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt7PowerIn();
+   updateMppt7Efficiency();
 }
 void MpptView::mppt7CurrentInReceived(double mppt7CurrentIn)
 {
    ui_.setMppt7CurrentIn().setNum(mppt7CurrentIn);
-   // ui_.setMppt7PowerIn().setNum(mppt7CurrentIn *
-      // ui_.setMppt7VoltageIn().text().toDouble());
-   // ui_.setMppt7Efficiency().setNum(ui_.setMppt7PowerIn().text().toDouble() *
-      // ui_.setMppt7PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt7PowerIn();
+   updateMppt7Efficiency();
 }
 void MpptView::mppt7VoltageOutReceived(double mppt7VoltageOut)
 {
    ui_.setMppt7VoltageOut().setNum(mppt7VoltageOut);
-   // ui_.setMppt7PowerOut().setNum(mppt7VoltageOut *
-      // ui_.setMppt7CurrentOut().text().toDouble());
-   // ui_.setMppt7Efficiency().setNum(ui_.setMppt7PowerIn().text().toDouble() *
-      // ui_.setMppt7PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt7PowerOut();
+   updateMppt7Efficiency();
 }
 void MpptView::mppt7CurrentOutReceived(double mppt7CurrentOut)
 {
    ui_.setMppt7CurrentOut().setNum(mppt7CurrentOut);
-   // ui_.setMppt7PowerOut().setNum(mppt7CurrentOut *
-      // ui_.setMppt7VoltageOut().text().toDouble());
-   // ui_.setMppt7Efficiency().setNum(ui_.setMppt7PowerIn().text().toDouble() *
-      // ui_.setMppt7PowerOut().text().toDouble() * 100); // in percentage
+   updateMppt7PowerOut();
+   updateMppt7Efficiency();
 }
 void MpptView::mppt7TypeReceived(QString mppt7Type)
 {
